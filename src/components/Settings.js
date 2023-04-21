d333ea72bc77eb40f8f49d3c480ee4162c559394
Kyle Grande <kyle.g.grande@gmail.com>
2023-04-21 10:48:58 -0500

    safety

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -18,6 +18,7 @@ const Settings = ({ apiKey, setApiKey }) => {
       <label htmlFor="api-key"><a href='https://platform.openai.com/account/api-keys'>OpenAI </a>API KEY</label>
       <input type="text" id="api-key" value={tempKey} onChange={handleChange} />
       <button onClick={handleSave}>Save</button>
+      <p className='safety'>We prioritize your data security by keeping your information locally stored and using secure, encrypted connections to OpenAI services. You can use our app with confidence, knowing that your notes and API key are safe and secure.</p>
     </div>
   );
 };
